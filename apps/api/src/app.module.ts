ec946ab69481722a96b5b22a23cec1da79b96a60
TEm <tem2k2.dev@gmail.com>
2025-06-01 11:25:24 +0700

    feat(api): add package graphql

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,20 @@ import { LinksModule } from './links/links.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [LinksModule, PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+    }),
+
+    LinksModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
