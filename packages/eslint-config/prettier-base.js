162c4c3b381f2ba1f659944bc9b4659d1c8cdd9e
TEm <tem2k2.dev@gmail.com>
2025-06-01 09:46:15 +0700

    refactor

--- packages/eslint-config/prettier-base.js
+++ packages/eslint-config/prettier-base.js
@@ -1,4 +1,21 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  vueIndentScriptAndStyle: false
+
 };
