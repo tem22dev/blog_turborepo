a1f94e87d89580bbf0257c3ffcc747a65773e043
TEm <tem2k2.dev@gmail.com>
2025-06-03 09:58:00 +0700

    feat(api): protecting GraphQL endpoints

---
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthJwtPayload } from '../types/auth-jwt-payload';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: AuthJwtPayload) {
+    const userId = payload.sub;
+    return this.authService.validateJwtUser(userId);
+  }
+}
